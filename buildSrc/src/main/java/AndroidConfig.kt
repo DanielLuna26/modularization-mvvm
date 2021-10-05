55e8b37c24a5641e1fe22044cbc817aac1c84965
Daniel Luna <daniel@macos.local>
2021-10-05 16:15:50 -0500

    initial commit

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,10 @@
+object AndroidConfig {
+    const val compileSdk = 31
+    const val applicationId = "com.softmoon.modularization_mvvm"
+    const val minSdk = 23
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "0.0.1"
+
+    const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
+}
